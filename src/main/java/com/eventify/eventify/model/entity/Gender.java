11c8be4b4b0dd5fb1528926c0ff429b0bd4d4419
brenopereira18 <brenopereira@gmail.com>
2025-04-02 16:47:39 -0300

    feat: create enum for user gender

---
+++ src/main/java/com/eventify/eventify/model/entity/Gender.java
@@ -0,0 +1,12 @@
+package com.eventify.eventify.model.entity;
+
+public enum Gender {
+    MASCULINO("Masculino"),
+    FEMININO("Feminino");
+
+    private String gender;
+
+    Gender(String gender) {
+        this.gender = gender;
+    }
+}
