69f2f65b2a05da57b15a9b84ee345eefc2b916c1
brenopereira18 <brenopereira@gmail.com>
2025-04-02 16:44:02 -0300

    feat: add annotations for class

--- src/main/java/com/eventify/eventify/model/entity/CashWithdrawalEntity.java
+++ src/main/java/com/eventify/eventify/model/entity/CashWithdrawalEntity.java
@@ -4,11 +4,21 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "cash_withdrawal")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CashWithdrawalEntity {
 
     @Id
@@ -18,7 +28,7 @@ public class CashWithdrawalEntity {
     @OneToOne(mappedBy = "cashWithdrawal", optional = false)
     private UserEntity user;
 
-    @Column(name = "amount_withdrawn" , nullable = false)
+    @Column(name = "amount_withdrawn" , nullable = false, columnDefinition = "DECIMAL(10,2) DEFAULT 0.00")
     @NotNull
     @DecimalMin(value = "20.00", message = "O valor mínimo para saque é de R$20,00")
     private BigDecimal amountWithdrawn;
