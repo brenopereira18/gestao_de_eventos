031531442cb1ff7035240e554661ea262ce63a31
brenopereira18 <brenopereira@gmail.com>
2025-05-13 17:34:02 -0300

    feat: add methods for event

--- src/main/java/com/eventify/eventify/module/event/model/entity/EventEntity.java
+++ src/main/java/com/eventify/eventify/module/event/model/entity/EventEntity.java
@@ -5,6 +5,7 @@ import com.eventify.eventify.module.sectorAndLot.model.entity.SectorEntity;
 import com.eventify.eventify.module.ticket.model.entity.DiscountCouponEntity;
 import com.eventify.eventify.module.user.model.entity.UserEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -55,7 +56,7 @@ public class EventEntity {
     private List<SectorEntity> sectors = new ArrayList<>();
 
     @OneToMany(mappedBy = "event", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<LotEntity> lots;
+    private List<LotEntity> lots = new ArrayList<>();
 
     @OneToMany(mappedBy = "event", cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
@@ -78,7 +79,10 @@ public class EventEntity {
     private UserEntity organizer;
 
     @Enumerated(EnumType.STRING)
-    private EventReview eventReview = EventReview.PENDING;
+    private StatusReview statusReview = StatusReview.PENDING;
+
+    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<EventReviewEntity> eventReviews = new ArrayList<>();
 
     @Future
     @Column(name = "sales_end_date", nullable = false)
@@ -92,4 +96,44 @@ public class EventEntity {
     @CreationTimestamp
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
+
+    @AssertTrue
+    public boolean isItAvailable() {
+        return !LocalDateTime.now().isAfter(this.startDate.plusHours(2));
+    }
+
+    public void addSector(SectorEntity sector) {
+        this.sectors.add(sector);
+        sector.setEvent(this);
+    }
+
+    public void removeSector(SectorEntity sector) {
+        this.sectors.remove(sector);
+        sector.setEvent(null);
+    }
+
+    public void addLot(LotEntity lot) {
+        this.lots.add(lot);
+        lot.setEvent(this);
+    }
+
+    public void removeLot(LotEntity lot) {
+        this.lots.remove(lot);
+        lot.setEvent(null);
+    }
+
+    public void addDiscountCoupon(DiscountCouponEntity coupon) {
+        this.discountCoupons.add(coupon);
+        coupon.setEvent(this);
+    }
+
+    public void removeDiscountCoupon(DiscountCouponEntity coupon) {
+        this.discountCoupons.remove(coupon);
+        coupon.setEvent(null);
+    }
+
+    public void addEventReview(EventReviewEntity review) {
+        this.eventReviews.add(review);
+        review.setEvent(this);
+    }
 }
