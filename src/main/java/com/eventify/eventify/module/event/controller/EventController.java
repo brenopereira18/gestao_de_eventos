ccbdc6edeaeac3f91bfc25d9765e14456a930226
brenopereira18 <brenopereira@gmail.com>
2025-05-26 11:53:41 -0300

    feat: create GET method to search for events

--- src/main/java/com/eventify/eventify/module/event/controller/EventController.java
+++ src/main/java/com/eventify/eventify/module/event/controller/EventController.java
@@ -1,16 +1,17 @@
 package com.eventify.eventify.module.event.controller;
 
-import com.eventify.eventify.module.event.model.dto.EventResponseDTO;
+import com.eventify.eventify.module.event.model.dto.CreateEventResponseDTO;
+import com.eventify.eventify.module.event.model.dto.GetEventResponseDTO;
+import com.eventify.eventify.module.event.model.entity.EventCategory;
 import com.eventify.eventify.module.event.service.EventService;
 import com.eventify.eventify.module.user.model.entity.UserEntity;
 import com.eventify.eventify.module.user.repository.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/event")
@@ -22,10 +23,30 @@ public class EventController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/")
-    public ResponseEntity<EventResponseDTO> createEvent(@Valid @RequestBody EventResponseDTO eventResponseDTO) {
+    @PostMapping
+    public ResponseEntity<CreateEventResponseDTO> createEvent(@Valid @RequestBody CreateEventResponseDTO createEventResponseDTO) {
         UserEntity user = userRepository.findByCpf("127.890.938-24").orElseThrow();
-        EventResponseDTO event = eventService.createEvent(eventResponseDTO, user);
+        CreateEventResponseDTO event = this.eventService.createEvent(createEventResponseDTO, user);
         return ResponseEntity.ok().body(event);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GetEventResponseDTO> getPublicEvent(@PathVariable Long id) {
+        GetEventResponseDTO eventResponse = this.eventService.getPublicEvent(id);
+        return ResponseEntity.ok().body(eventResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GetEventResponseDTO>> getAllEvents(
+        @RequestParam(required = false) String name,
+        @RequestParam(required = false) String city,
+        @RequestParam(required = false) EventCategory eventCategory
+        ) {
+        List<GetEventResponseDTO> events = this.eventService.getAllEvents(name, city, eventCategory);
+
+        if (events.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok().body(events);
     }
 }
