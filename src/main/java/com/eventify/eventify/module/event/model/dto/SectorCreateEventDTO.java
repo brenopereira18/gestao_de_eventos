a238a05b577f2d448abe62a33df7bf269b3d49ae
brenopereira18 <brenopereira@gmail.com>
2025-05-27 18:39:09 -0300

    remove: exclude file

--- src/main/java/com/eventify/eventify/module/event/model/dto/SectorCreateEventDTO.java
+++
@@ -1,4 +0,0 @@
-package com.eventify.eventify.module.event.model.dto;
-
-public record SectorCreateEventDTO(Long id, String name) {
-}
--- src/main/java/com/eventify/eventify/module/event/repository/FinancialOfTheEventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eventify.eventify.module.event.repository;
-
-import com.eventify.eventify.module.event.model.entity.FinancialOfTheEventEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FinancialOfTheEventRepository extends JpaRepository<FinancialOfTheEventEntity, Long> {
-}
